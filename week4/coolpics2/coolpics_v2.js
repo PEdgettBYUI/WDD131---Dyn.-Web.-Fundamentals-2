f2d9be9773ff68f242a4996fac49914670f15a75
Patrick Edgett <edg24004@byui.edu>
2026-02-03 16:44:21 -0700

    Coolpics_v2 complete; added coolpics1 & coolpics2 to homepage; renamed folder in week4 from 'coolpics' to 'coolpics2';

---
+++ week4/coolpics2/coolpics_v2.js
@@ -0,0 +1,45 @@
+/* Menu Controls */
+var menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+console.log(menuBtn);
+console.log(menu);
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle("hide");
+    menuBtn.classList.toggle("change");
+}
+
+/* Gallery Modal Controls */
+const gallery = document.querySelector('.gallery');
+const modal = document.querySelector('dialog');
+const modalImage = modal.querySelector('img');
+const closeButton = modal.querySelector('.close-viewer');
+
+// Event listener for opening the modal
+gallery.addEventListener('click', openModal);
+
+function openModal(e) {
+// Code to show modal  - Use event parameter 'e'   
+    const image = e.target;
+    const src = image.getAttribute('src');
+    const alt = image.getAttribute('alt');
+    const largeSrc = src.replace('-sm', '-full');
+    modalImage.setAttribute('src', largeSrc);
+    modalImage.setAttribute('alt', alt);
+    modal.showModal();
+}
+// Close modal on button click
+closeButton.addEventListener('click', () => {
+    modal.close();
+});
+
+// Close modal if clicking outside the image
+modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.close();
+    }
+});
+
+
