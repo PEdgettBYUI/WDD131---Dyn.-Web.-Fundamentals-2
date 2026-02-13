4bf37fe4373a9eb947eda20fb78ec65bc830beb8
Patrick Edgett <edg24004@byui.edu>
2026-02-12 17:36:47 -0700

    FInally fixed mission statement v2; link to script was misspelled;

--- week2/mission2/mission2.js
+++ week2/mission2/mission2.js
@@ -1,34 +1,20 @@
-const themeSelector = document.getElementById("themeSelector");
-// replace with code to select dropdown element out of the HTML (Hint: document.querySelector)
 
+let selectElem = document.querySelector('select');
+let logo = document.querySelector('img');
+let pageContent = document.body;
 
+selectElem.addEventListener('change', changeTheme);
 
 function changeTheme() {
-// check to see what the current value of our select is.
-// The current value is conveniently found in themeSelector.value!
-const selectedTheme = themeSelector.value;
-const body = document.body;
-const logo = document.getElementById('logo');
-
-// if the value is dark then:
-if(selectedTheme === 'dark') {
-// add the dark class to the body
-    body.classList.add('dark');
-// change the source of the logo img to point to the white logo.
-    logo.src = 'byui-logo_white.png';
-}
-// otherwise
-else {
-// remove the dark class
-    body.classList.remove('dark');
-// make sure the logo src is the blue logo.
-    logo.src = 'byui-logo_blue.webp';
-}
-
-
-
-}
-
-// add an event listener to the themeSelector element here.
-// Use the changeTheme function as the event handler function.
-themeSelector.addEventListener('change', changeTheme);
+    let current = selectElem.value;
+    if (current == 'dark') {
+        pageContent.style.backgroundColor = "black";
+        pageContent.style.color = "white";
+        logo.src="byui-logo-white.png";
+    } else {
+        pageContent.style.backgroundColor = "white";
+        pageContent.style.color = "black";
+        logo.src="byui-logo-blue.webp";
+    }
+}           
+                    
